2ec9dce3c25431bfb120556afe12a199970f1295
Jonny Moore <jonnymoore121212@gmail.com>
2016-08-15 22:52:39 +0100

    Extracts code to separate models / spec folders

--- spec/note-tests.js
+++ spec/note-tests.js
@@ -1,9 +1,7 @@
 var assert = require("./assert.js")
-var Note = require("./note-model.js")
+var Note = require("../models/note-model.js")
 
 function testNoteCanStoreText(){
   var note = new Note("My favourite language is JavaScript");
   assert.isTrue(note.text === "My favourite language is JavaScript");
 };
-
-testNoteCanStoreText();
