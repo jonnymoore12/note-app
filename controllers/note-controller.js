de97f1265a9f24fbfac3ff8aba7688750531d5e4
Bart <kluczynskib@gmail.com>
2016-08-17 16:43:33 +0200

    add the functionality of a random note being displayed in the browser

--- controllers/note-controller.js
+++ controllers/note-controller.js
@@ -1,6 +1,23 @@
-function changeText(text){
-  element = document.getElementById("app");
-  element.innerHTML = text;
+// function changeText(text){
+//   element = document.getElementById("app");
+//   element.innerHTML = text;
+// };
+//
+// changeText("howdy");
+
+(function(exports){
+
+function NoteController(){
+var noteList = new NoteList();
+noteList.createNote('Favourite drink: seltzer')
+this.noteListView = new NoteListView(noteList);
 };
 
-changeText("howdy");
+NoteController.prototype.displayNotes = function(){
+  var element = document.getElementById('app');
+  element.innerHTML = this.noteListView.viewNotes();
+}
+
+exports.NoteController = NoteController;
+
+})(this);
--- models/note-list-model.js
+++ models/note-list-model.js
@@ -1,5 +1,4 @@
 // var Note = require("./note-model.js").Note;
-var new_note = new Note("test");
 
 (function(exports) {
   function NoteList() {
